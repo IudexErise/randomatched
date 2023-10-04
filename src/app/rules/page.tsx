6b313a7ac4b0ab772926a49c09fad16c3f50c59d
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-10-04 19:43:08 +0600

    [chore]: rules files

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -2,13 +2,25 @@
 
 import Header from './../../components/header/header';
 import Footer from './../../components/footer/footer';
+import styles from './page.module.scss';
+import Link from 'next/link';
 
-export default function Rules () {
+export default function Rules() {
   return (
     <>
-    <Header />
-    <h1>Rules</h1>
-    <Footer />
+      <Header />
+      <main className={styles.main}>
+        <ul>
+          Правила наборов для 4 игроков
+        </ul>
+        <ul>
+          Правила наборов для 3 игроков
+        </ul>
+        <ul>
+          Правила наборов для 2 игроков
+        </ul>
+      </main>
+      <Footer />
     </>
   )
 }
