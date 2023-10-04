ab2e65a5f2f7a62c41e24c00461d08494593b0de
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-10-04 22:10:19 +0600

    [style]: responsive navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,19 +5,24 @@ import styles from './header.module.scss';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import logo from '../../../public/logo.png';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
 
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState('');
+  useEffect(() => {
+    setCurrentPage(window.location.pathname);
+  }, [])
 
   return (
     <header className={styles.header}>
       <Image src={logo} alt='Логотип' onClick={() => router.push('/')} />
       <nav>
-        <Link href='/'>Главная</Link>
-        <Link href='/sets'>Наборы</Link>
-        <Link href='/manual'>Ручной выбор</Link>
-        <Link href='/rules'>Правила</Link>
+        <Link href='/' className={currentPage === '/' ? styles.linkActive : styles.link}>Главная</Link>
+        <Link href='/sets' className={currentPage === '/sets' ? styles.linkActive : styles.link}>Наборы</Link>
+        <Link href='/manual' className={currentPage === '/manual' ? styles.linkActive : styles.link}>Ручной выбор</Link>
+        <Link href='/rules' className={currentPage === '/rules' ? styles.linkActive : styles.link}>Правила</Link>
       </nav>
     </header>
   )
