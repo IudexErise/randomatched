9b2fed0d936f2cdad8a01cf9b44c488af9e8d594
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-10-12 18:23:46 +0600

    [style]: mobile footer + rules

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,14 +6,20 @@ import styles from './footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <nav>
-        <Link href='/'>Главная</Link>
-        <Link href='/sets'>Наборы</Link>
-        <Link href='/manual'>Ручной выбор</Link>
-        <Link href='/rules'>Правила</Link>
-        <Link href='/mysets'>Мои наборы</Link>
-      </nav>
-      <p>Сайт-помощник для случайного выбора персонажа и поля для настольной игры Unmatched!</p>
+      <div className={styles.content}>
+        <nav>
+          <Link href='/' className={styles.link}>Главная</Link>
+          <Link href='/sets' className={styles.link}>Наборы</Link>
+          <Link href='/manual' className={styles.link}>Ручной выбор</Link>
+          <Link href='/rules' className={styles.link}>Правила</Link>
+          <Link href='/mysets' className={styles.link}>Мои наборы</Link>
+        </nav>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>Сайт-помощник для случайного выбора персонажа и поля для настольной игры Unmatched</p>
+          <p className={styles.text}>Все права на продукт принадлежат Reasotation Games, Mondo Games, Iello и Gaga Games</p>
+        </div>
+      </div>
+      <Link href='https://www.linkedin.com/in/iudexerise/' className={styles.myLink}>V.Saprykin 2023©</Link>
     </footer>
   )
 }
