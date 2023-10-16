e0b834710391cc92696af867f0ac2f83b539d88f
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-10-16 19:47:16 +0600

    [feat]: rules local

--- src/app/[locale]/rules/page.tsx
+++ src/app/[locale]/rules/page.tsx
@@ -4,12 +4,19 @@ import Header from '../../../components/header/header';
 import Footer from '../../../components/footer/footer';
 import styles from './page.module.scss';
 import Link from 'next/link';
-import { rulesData } from '@/data/rulesData';
+import { rulesDataProps, rulesDataRu } from '@/data/rulesDataRu';
+import { rulesDataEn } from '@/data/rulesDataEn';
+import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
 
 export default function Rules() {
 
+  const [rulesData, setRulesData] = useState<any>(rulesDataEn);
+
+  const t = useTranslations('pages.rules');
+
   const sortRules = (players: number) => {
-    let rules = rulesData.filter((item) => item.playersCount === players).map((rule) => {
+    let rules = rulesData.filter((item : any) => item.playersCount === players).map((rule : any) => {
       return (
         <Link
           target='_blank'
@@ -28,22 +35,30 @@ export default function Rules() {
   let rules3 = sortRules(3);
   let rules2 = sortRules(2);
 
+  useEffect(() => {
+    if (window.location.pathname.substring(1, 3) === 'ru' ){
+      setRulesData(rulesDataRu)
+    } else {
+      setRulesData(rulesDataEn)
+    }
+  }, [])
+
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h1>Правила</h1>
+        <h1>{t('pageName')}</h1>
         <div className={styles.rules}>
           <div className={styles.category}>
-            <h2>Наборы для 4 игроков</h2>
+            <h2>{t('category4')}</h2>
             {rules4}
           </div>
           <div className={styles.category}>
-            <h2>Наборы для 3 игроков</h2>
+            <h2>{t('category3')}</h2>
             {rules3}
           </div>
           <div className={styles.category}>
-            <h2>Наборы для 2 игроков</h2>
+            <h2>{t('category2')}</h2>
             {rules2}
           </div>
         </div>
