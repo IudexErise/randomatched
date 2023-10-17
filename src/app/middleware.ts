5a17b33ad82c894eaa6694c8886296e2ea23bf3b
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-10-17 21:28:14 +0600

    [chore]: minor changes

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,9 +2,10 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   locales: ['en', 'ru'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localePrefix: 'always'
 });
  
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
