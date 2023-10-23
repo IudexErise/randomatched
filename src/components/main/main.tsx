bac2dd08322a1f1a202ecc1e4fb4f6e178013303
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-10-23 15:48:27 +0600

    [fix]: main page

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -26,39 +26,41 @@ export default function Main({ locale }: MainProps) {
 
   return (
     <main className={styles.main}>
-      <div className={styles.item} onClick={() => router.push(t('redir1'))}>
-        {locale === 'ru' ?
-          <Image src={vol1ru} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-          :
-          <Image src={vol1en} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-        }
-        <h2 className={styles.headline}>{t('opt1')}</h2>
+      <div className={styles.container}>
+        <div className={styles.item} onClick={() => router.push(t('redir1'))}>
+          {locale === 'ru' ?
+            <Image src={vol1ru} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+            :
+            <Image src={vol1en} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+          }
+          <h2 className={styles.headline}>{t('opt1')}</h2>
+        </div>
+        <div className={styles.item} onClick={() => router.push(t('redir2'))}>
+          {locale === 'ru' ?
+            <Image src={vol2ru} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+            :
+            <Image src={vol2en} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+          }
+          <h2 className={styles.headline}>{t('opt2')}</h2>
+        </div>
+        <div className={styles.item} onClick={() => router.push(t('redir3'))}>
+          {locale === 'ru' ?
+            <Image src={rulesRu} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+            :
+            <Image src={rulesEn} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+          }
+          <h2 className={styles.headline}>{t('opt3')}</h2>
+        </div>
+        <div className={styles.item} onClick={() => router.push(t('redir4'))}>
+          {locale === 'ru' ?
+            <Image src={mysets} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
+            :
+            <Image src={mysetsEn} alt='' priority sizes='(min-width: 768px) 400px, (max-width: 767px) 250px' />
+          }
+          <h2 className={styles.headline}>{t('opt4')}</h2>
+        </div>
       </div>
-      <div className={styles.item} onClick={() => router.push(t('redir2'))}>
-        {locale === 'ru' ?
-          <Image src={vol2ru} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-          :
-          <Image src={vol2en} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-        }
-        <h2 className={styles.headline}>{t('opt2')}</h2>
-      </div>
-      <div className={styles.item} onClick={() => router.push(t('redir3'))}>
-        {locale === 'ru' ?
-          <Image src={rulesRu} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-          :
-          <Image src={rulesEn} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-        }
-        <h2 className={styles.headline}>{t('opt3')}</h2>
-      </div>
-      <div className={styles.item} onClick={() => router.push(t('redir4'))}>
-        {locale === 'ru' ?
-          <Image src={mysets} alt='' priority sizes='(min-width: 768px) 300px, (max-width: 767px) 250px' />
-          :
-          <Image src={mysetsEn} alt='' priority sizes='(min-width: 768px) 400px, (max-width: 767px) 250px' />
-        }
-        <h2 className={styles.headline}>{t('opt4')}</h2>
-      </div>
-        <Link href={t('pwaLink')} target='_blank'>{t('instruction')}</Link>
+      <Link href={t('pwaLink')} target='_blank' className={styles.headline}>{t('instruction')}</Link>
     </main>
   )
 }
